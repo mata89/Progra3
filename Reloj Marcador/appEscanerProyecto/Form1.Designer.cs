22b0ebee825b3899de2a72013b8835f289808885
mata89 <jemd_89.mata@hotmail.com>
2013-07-18 00:05:34 -0600

    Aplicación Reloj Marcador

---
+++ Reloj Marcador/appEscanerProyecto/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace appEscanerProyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCodigoEmpleado = new System.Windows.Forms.Label();
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.Font = new System.Drawing.Font("Showcard Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblHora.Location = new System.Drawing.Point(46, 61);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(215, 40);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "06 : 40 : 45 ";
+            this.lblHora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblCodigoEmpleado
+            // 
+            this.lblCodigoEmpleado.AllowDrop = true;
+            this.lblCodigoEmpleado.Location = new System.Drawing.Point(47, 123);
+            this.lblCodigoEmpleado.Name = "lblCodigoEmpleado";
+            this.lblCodigoEmpleado.Size = new System.Drawing.Size(214, 16);
+            this.lblCodigoEmpleado.TabIndex = 2;
+            this.lblCodigoEmpleado.Text = "Código de empleado";
+            this.lblCodigoEmpleado.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.Location = new System.Drawing.Point(83, 26);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(148, 35);
+            this.lblSaludo.TabIndex = 3;
+            this.lblSaludo.Text = "WELCOME";
+            this.lblSaludo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.Location = new System.Drawing.Point(107, 176);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(100, 16);
+            this.lblFecha.TabIndex = 4;
+            this.lblFecha.Text = "DD/MM/AAAA";
+            this.lblFecha.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 275);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblSaludo);
+            this.Controls.Add(this.lblCodigoEmpleado);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblHora);
+            this.Name = "Form1";
+            this.Text = "Reloj Marcador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCodigoEmpleado;
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
