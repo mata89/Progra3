64171bc0d69be75c1b9c0677dc40939df40b5d41
mata89 <jemd_89.mata@hotmail.com>
2013-07-24 19:10:24 -0600

    Revert "elimine el proyecto anterior e ingrese el nuevo"
    This reverts commit 3308972521c70ff9970c2ef1441715a0d6987ee3.

--- Reloj Marcador/appEscanerProyecto/Form1.Designer.cs
+++ Reloj Marcador/appEscanerProyecto/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace GUI
+﻿namespace appEscanerProyecto
 {
-    partial class frmRelojMarcador
+    partial class Form1
     {
         /// <summary>
         /// Variable del diseñador requerida.
@@ -30,41 +30,37 @@
         {
             this.components = new System.ComponentModel.Container();
             this.lblHora = new System.Windows.Forms.Label();
-            this.txtIdEmpleado = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblCodigoEmpleado = new System.Windows.Forms.Label();
             this.lblSaludo = new System.Windows.Forms.Label();
             this.lblFecha = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // lblHora
             // 
-            this.lblHora.Font = new System.Drawing.Font("Franklin Gothic Demi", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHora.ForeColor = System.Drawing.Color.Green;
-            this.lblHora.Location = new System.Drawing.Point(52, 79);
+            this.lblHora.Font = new System.Drawing.Font("Showcard Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblHora.Location = new System.Drawing.Point(46, 61);
             this.lblHora.Name = "lblHora";
-            this.lblHora.Size = new System.Drawing.Size(208, 44);
+            this.lblHora.Size = new System.Drawing.Size(215, 40);
             this.lblHora.TabIndex = 0;
-            this.lblHora.Text = " :  :  ";
+            this.lblHora.Text = "06 : 40 : 45 ";
             this.lblHora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblHora.Click += new System.EventHandler(this.lblHora_Click);
             // 
-            // txtIdEmpleado
+            // textBox1
             // 
-            this.txtIdEmpleado.Location = new System.Drawing.Point(52, 150);
-            this.txtIdEmpleado.Name = "txtIdEmpleado";
-            this.txtIdEmpleado.Size = new System.Drawing.Size(208, 20);
-            this.txtIdEmpleado.TabIndex = 1;
-            this.txtIdEmpleado.TextChanged += new System.EventHandler(this.txtIdEmpleado_TextChanged);
-            this.txtIdEmpleado.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Location = new System.Drawing.Point(46, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // lblCodigoEmpleado
             // 
             this.lblCodigoEmpleado.AllowDrop = true;
-            this.lblCodigoEmpleado.Location = new System.Drawing.Point(52, 127);
+            this.lblCodigoEmpleado.Location = new System.Drawing.Point(47, 123);
             this.lblCodigoEmpleado.Name = "lblCodigoEmpleado";
-            this.lblCodigoEmpleado.Size = new System.Drawing.Size(208, 20);
+            this.lblCodigoEmpleado.Size = new System.Drawing.Size(214, 16);
             this.lblCodigoEmpleado.TabIndex = 2;
             this.lblCodigoEmpleado.Text = "Código de empleado";
             this.lblCodigoEmpleado.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -72,25 +68,21 @@
             // lblSaludo
             // 
             this.lblSaludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSaludo.Location = new System.Drawing.Point(12, 26);
+            this.lblSaludo.Location = new System.Drawing.Point(83, 26);
             this.lblSaludo.Name = "lblSaludo";
-            this.lblSaludo.Size = new System.Drawing.Size(283, 35);
+            this.lblSaludo.Size = new System.Drawing.Size(148, 35);
             this.lblSaludo.TabIndex = 3;
-            this.lblSaludo.Text = "BIENVENIDO";
-            this.lblSaludo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSaludo.Text = "WELCOME";
             this.lblSaludo.Click += new System.EventHandler(this.label1_Click);
             // 
             // lblFecha
             // 
-            this.lblFecha.Font = new System.Drawing.Font("Franklin Gothic Demi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFecha.ForeColor = System.Drawing.Color.Green;
-            this.lblFecha.Location = new System.Drawing.Point(106, 182);
+            this.lblFecha.Location = new System.Drawing.Point(107, 176);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(102, 20);
+            this.lblFecha.Size = new System.Drawing.Size(100, 16);
             this.lblFecha.TabIndex = 4;
             this.lblFecha.Text = "DD/MM/AAAA";
             this.lblFecha.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lblFecha.TextChanged += new System.EventHandler(this.timer1_Tick);
             // 
             // timer1
             // 
@@ -98,22 +90,17 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // timer2
-            // 
-            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
-            // 
-            // frmRelojMarcador
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(307, 223);
+            this.ClientSize = new System.Drawing.Size(307, 275);
             this.Controls.Add(this.lblFecha);
             this.Controls.Add(this.lblSaludo);
             this.Controls.Add(this.lblCodigoEmpleado);
-            this.Controls.Add(this.txtIdEmpleado);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lblHora);
-            this.Name = "frmRelojMarcador";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Name = "Form1";
             this.Text = "Reloj Marcador";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -124,12 +111,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblHora;
-        private System.Windows.Forms.TextBox txtIdEmpleado;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label lblCodigoEmpleado;
         private System.Windows.Forms.Label lblSaludo;
         private System.Windows.Forms.Label lblFecha;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Timer timer2;
     }
 }
 
